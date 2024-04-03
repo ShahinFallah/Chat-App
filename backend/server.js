c5dfbce1c2053c83b4456aa54feab55fa38d4bd3
Ashkan <AshkanHagh01@gmail.com>
2024-04-03 19:12:09 +0330

    completed - backend part

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import CookieParser from 'cookie-parser';
 
 import authRouter from './routes/auth.js';
+import messageRouter from './routes/message.js';
+import userRouter from './routes/user.js';
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -10,8 +14,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads
+app.use(CookieParser());
 
 app.use('/api/auth', authRouter);
+app.use('/api/messages', messageRouter);
+app.use('/api/users', userRouter);
 
 
 app.listen(PORT, () => {
