37476576a34e9b95bbe0b969b6fa38e3c8b5f39e
Ashkan <AshkanHagh01@gmail.com>
2024-04-03 17:03:27 +0330

    Completed the authentication Controller and connected to the DB

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectToMongoDB = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI);
+
+        console.log('Connected to MongoDB');
+
+    } catch (error) {
+        
+        console.log('error connecting to MongoDB', error.message);
+    }
+
+}
+
+export default connectToMongoDB
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import authRouter from './routes/auth.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
+
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+app.use(express.json()); // to parse the incoming requests with JSON payloads
+
+app.use('/api/auth', authRouter);
+
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server is running on port ${PORT}`);
+});
