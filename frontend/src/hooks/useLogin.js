2f3bd6bf16b6717e3b928a8bdd2ce3240171465e
ShahinFallah <shahinfallah2006@gmail.com>
2024-04-05 22:52:32 +0330

    fix

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -22,7 +22,7 @@ const useLogin = () => {
                 throw new Error(data.error)
             }
 
-            localStorage.setItem("chat-item", JSON.stringify(data))
+            localStorage.setItem("chat-user", JSON.stringify(data))
             setAuthUser(data)
 
 
