2e7aae6cd7271792961934139a2bf285d0967939
Ashkan <AshkanHagh01@gmail.com>
2024-04-07 20:37:14 +0330

    backend part completed - socket

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -1,5 +1,6 @@
 import Conversation from '../models/conversation.js';
 import Message from '../models/message.js';
+import { getReceiverSocketId, io } from '../socket/socket.js';
 
 export const sendMessage = async (req, res) => {
 
@@ -26,6 +27,12 @@ export const sendMessage = async (req, res) => {
         }
 
         await Promise.all([conversation.save(), newMessage.save()]);
+
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+
+            io.to(receiverSocketId).emit('newMessage', newMessage);
+        }
 
         res.status(201).json(newMessage);
 
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,12 +12,26 @@ const io = new Server(server, {
     }
 });
 
+export const getReceiverSocketId = (receiverId) => {
+    return userSocketMap[receiverId];
+}
+
+const userSocketMap = {}; //{userId: socketId};
+
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
+
+    const userId = socket.handshake.query.userId;
+    if(userId != 'undefined') userSocketMap[userId] = socket.id;
+
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
 
 
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id);
+
+        delete userSocketMap[userId];
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
     });
 });
 
