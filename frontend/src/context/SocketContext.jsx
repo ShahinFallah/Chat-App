c9d36ae6709da2db5594d71ce7fd2d1a6981ecc3
ShahinFallah <shahinfallah2006@gmail.com>
2024-04-08 23:50:01 +0330

    fix

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io('http://localhost:5000', {
+            const socket = io('https://chatapp-5jbb.onrender.com', {
                 query: {
                     userId: authUser._id
                 }
