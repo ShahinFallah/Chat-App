a3a97aecc85946b961d09b73e898cf6251b25b00
ShahinFallah <shahinfallah2006@gmail.com>
2024-04-12 17:52:26 +0330

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import SignUp from './pages/signup/SignUp'
+import SignUp from './pages/signUp/SignUp'
 
 function App() {
   return (
