23481f1dc4b036f83d85a3ec64b4c727a5838bb5
ShahinFallah <shahinfallah2006@gmail.com>
2024-04-26 11:52:59 +0330

    fix

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,13 +4,14 @@ import MessagesContainer from '../../components/messages/MessagesContainer'
 
 function Home() {
   return (
-    <div className="flex w-full home-bg-color h-screen bg-background rounded-lg border border-primary_200
-    sm:p-7
-    sm:w-[1300px] 
-    sm:h-[85vh]
-    ">
-      <Sidebar />
-      <MessagesContainer />
+    <div className="h-screen py-20">
+      <div className="flex w-full h-full home-bg-color bg-background rounded-lg border border-primary_200
+      sm:p-7
+      sm:w-[70vw]
+      ">
+        <Sidebar />
+        <MessagesContainer />
+      </div>
     </div>
   )
 }
