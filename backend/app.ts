eae07e8493985eb4c1e0aa5170eed054dacdef49
Ashkan <AshkanHagh01@gmail.com>
2024-04-28 22:16:52 +0330

    Fix & Update

--- backend/app.ts
+++ backend/app.ts
@@ -1,20 +1,15 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
-
 import { app, server } from './socket/socket';
-
+import cookieParser from 'cookie-parser';
 import connectDB from './db/connectDB';
 
 import authRoute from './routes/auth.route';
 import userRoute from './routes/user.route';
 import conversation from './routes/conversation.route';
 import messagesRoute from './routes/message.route';
-import faker from './utils/faker';
-
 
 const PORT = process.env.PORT || 5500;
 connectDB();
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,7 +19,6 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/conversation', conversation);
 app.use('/api/messages', messagesRoute);
-app.use('/faker', faker) // add fake user
 
 
 server.listen(PORT, () : void => console.log(`server is running on port ${PORT}`));
--- backend/db/connectDB.ts
+++ backend/db/connectDB.ts
@@ -5,11 +5,12 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_DB_URI);
 
-        console.log(`DB is connected on ${conn.connection.host}`);
+        console.log(`DATABASE is connected on ${conn.connection.host}`);
 
     } catch (error) {
         
-        console.log('failed to connect DB :', error);   
+        console.log('failed to connect DB :', error);
+        process.exit(1);
     }
 
 }
