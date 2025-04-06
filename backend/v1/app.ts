986b15d0f2d8677337df93c189bdf4cf0d09d743
Ashkan <AshkanHagh01@gmail.com>
2025-04-06 21:59:52 +0330

    feat: add CORS support

--- backend/v1/app.ts
+++ backend/v1/app.ts
@@ -1,26 +1,33 @@
-import express from 'express';
-import { app, server } from './socket/socket';
-import cookieParser from 'cookie-parser';
-import connectDB from './db/connectDB';
+import express from "express";
+import { app, server } from "./socket/socket";
+import cookieParser from "cookie-parser";
+import connectDB from "./db/connectDB";
 
-import authRoute from './routes/auth.route';
-import userRoute from './routes/user.route';
-import conversation from './routes/conversation.route';
-import messagesRoute from './routes/message.route';
+import authRoute from "./routes/auth.route";
+import userRoute from "./routes/user.route";
+import conversation from "./routes/conversation.route";
+import messagesRoute from "./routes/message.route";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5500;
 connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN!,
+  }),
+);
 
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/conversation', conversation);
-app.use('/api/messages', messagesRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/conversation", conversation);
+app.use("/api/messages", messagesRoute);
 
-
-server.listen(PORT, () : void => console.log(`server is running on port ${PORT}`));
+server.listen(PORT, (): void =>
+  console.log(`server is running on port ${PORT}`),
+);
 
 export default app;
