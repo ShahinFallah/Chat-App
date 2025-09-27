2b1d91a68b4a98bc4b21e07328f3d4d3fbbcfc73
Ashkan <AshkanHagh01@gmail.com>
2025-09-27 23:10:24 +0330

    chore(socket): update server socket origin

--- backend/v1/socket/socket.ts
+++ backend/v1/socket/socket.ts
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.ORIGIN],
+    origin: process.env.ORIGIN,
   },
 });
 
