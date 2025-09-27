66125d655d867dde8432f6ad2c6845c962a95cd3
ShahinFallah <shahinfallah2006@gmail.com>
2025-09-28 01:17:23 +0330

    fix: correct Content-Type header in axios instance configuration

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    "Content-Type": "json/application"
+    "Content-Type": "application/json"
   },
   withCredentials: true
 })
